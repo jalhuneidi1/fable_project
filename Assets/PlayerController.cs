26994cedc882a26ce1c71a5e739e9c9ea3175455
Jana AlHuneidi <jalhuneidi1@gmail.com>
2023-09-25 17:18:17 +0300

    Character movement animation, updated character input controll code

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
 
@@ -18,6 +20,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -25,22 +29,61 @@ public class PlayerController : MonoBehaviour
         // if movement  is not 0, try not to move
         if (movementInput != Vector2.zero)
         {
-            /*
-            movementInput (direction) - X and Y values between - 1 and 1 that reprsent the direction from the body to look for collisions
-            movementFilter - The settings that determine where a collision can occur on such as layers to collide with 
-            castCollisions - List of collisions to store the found collisons inyo after the Cast is finished
-            moveSpeed * Time.fixedDeltaTime + collisionsOffset - The ammount to cast equal to the movement plus the offset (dont get stuck in the wall), time between the frame (fixed deltatime)
-            */
-            int count = rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+           bool success = TryMove(movementInput);
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+
+            animator.SetBool("isMoving", success);
+        } else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        // set movement of sprite to movement direction
+        if(movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        } else if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+
+
+    }
+    private bool TryMove(Vector2 direction) {
+        if (direction != Vector2.zero)
+        {
+            // check for potential collisions
+            int count = rb.Cast(
+                direction, // movementInput (direction) - X and Y values between - 1 and 1 that reprsent the direction from the body to look for collisions
+                movementFilter, // movementFilter - The settings that determine where a collision can occur on such as layers to collide with 
+                castCollisions, // castCollisions - List of collisions to store the found collisons into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // moveSpeed * Time.fixedDeltaTime + collisionsOffset - The ammount to cast equal to the movement plus the offset (dont get stuck in the wall), time between the frame (fixed deltatime)
 
             if (count == 0)
             {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
             }
-
+            else
+            {
+                return false;
+            }
         }
-
-    }
+        else
+        {
+            //can't move if there is no direction to move in
+            return false;
+        }
+        }
 
     void OnMove(InputValue movementValue)
     {
